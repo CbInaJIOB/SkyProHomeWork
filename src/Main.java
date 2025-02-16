a367e1e169e18098884910060750552bc7a35f2d
Admin <sypalov_sergey@mail.ru>
2025-02-16 16:15:05 +0300

    Add homeWork9

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("HelloWorld");
+
         //1
         System.out.println("Задача 1:");
         int cost[] = {123, 456, 789, 321, 345};
