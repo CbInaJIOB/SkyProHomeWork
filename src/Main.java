378d879f7d524d8c225a8fa911bde7bad3eb5d30
Admin <sypalov_sergey@mail.ru>
2025-02-16 18:04:31 +0300

    edit 2

--- src/Main.java
+++ src/Main.java
@@ -23,5 +23,26 @@ public class Main {
         System.out.println(fullName1.replace('ё', 'е'));
 
 
+
+        //1
+        System.out.println("Задача 1:");
+
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " +  middleName;
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
+        System.out.println();
+
+        //2
+        System.out.println("Задача 2:");
+        System.out.println(fullName.toUpperCase());
+        System.out.println();
+
+        //3
+        System.out.println("Задача 3:");
+        String fullName1 = "Иванов Семён Семёнович";
+        System.out.println(fullName1.replace('ё', 'е'));
+
     }
 }
